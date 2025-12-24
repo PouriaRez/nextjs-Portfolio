bb2b8aa6a25a5f0103718ca1005f0162df9bf834
PouriaRez <rezaeizadehpouria@gmail.com>
2025-12-23 17:23:57 -0800

    Working on home page

--- components/Homepage.tsx
+++ components/Homepage.tsx
@@ -13,17 +13,23 @@ const Homepage = () => {
           alt="Pouria Rezaei"
           width={500}
           height={500}
-          className="rounded-full shadow-[0_0_50px_rgba(168,85,247,0.6)] hover:shadow-[0_0_70px_rgba(168,85,247,0.6)] transition duration-200"
+          className="rounded-full shadow-[0_0_50px_#1D84B5] hover:shadow-[0_0_70px_#1D84B5] transition duration-200"
         />
       </div>
 
       <div>
         <div>
-          <div className="text-9xl">Pouria</div>
-          <div className="text-7xl italic">Fullstack Developer.</div>
+          {/* maybe remove linear gradient */}
+          <div className="text-7xl flex justify-center items-center md:justify-start md:text-9xl bg-linear-to-r from-[#1D84B5] rounded-2xl">
+            Pouria
+          </div>
+          <div className="text-6xl md:text-7xl italic">
+            Fullstack Developer.
+          </div>
         </div>
         <div>
-          <TechStack />
+          tech stack here.
+          {/* <TechStack /> */}
         </div>
       </div>
     </div>
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -45,10 +45,7 @@ const Navbar = () => {
         {!open && (
           <div>
             <p className="text-4xl hover:cursor-pointer font-bold">
-              Pouria{' '}
-              <span className="bg-linear-to-r from-purple-400 to-pink-500 bg-clip-text text-transparent">
-                Rezaei
-              </span>
+              Pouria Rezaei
             </p>
           </div>
         )}
