75b9444e802097337e4258401e121cbf87d06ccb
Pouria Rezaei <rezaeizadehpouria@gmail.com>
2026-01-11 19:37:07 -0800

    Working on tech card

--- components/TechnologyCard.tsx
+++ components/TechnologyCard.tsx
@@ -1,5 +1,42 @@
 const TechnologyCard = () => {
-  return <div>TechnologyCard</div>;
+  return <div></div>;
 };
 
 export default TechnologyCard;
+/*
+Languages
+JavaScript
+TypeScript
+Python
+HTML
+CSS / SCSS
+C++
+C
+
+Frameworks & Libraries
+React
+React Native
+Angular
+MUI (Material UI)
+
+Backend & APIs
+Node.js
+Express
+PostgreSQL
+Firebase
+OpenAPI
+Plaid API
+
+Developer Tools & Testing
+Git
+GitHub
+Docker
+Vitest
+React Testing Library
+MSW (Mock Service Worker)
+Cloud & Deployment
+
+Vercel
+Render
+
+*/
